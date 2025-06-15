5e40d57bceda6a4037ab7f21d0f646eacf46847d
Olivia Jardine <oliviafjardine@gmail.com>
2025-06-15 16:56:30 -0700

    image optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
-  assetsInclude: ['**/*.mp4'],
-  build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name?.split('.') || [];
-          const extType = info[info.length - 1];
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            return `assets/images/[name]-[hash][extname]`;
-          }
-          if (/mp4|webm|ogg|mp3|wav|flac|aac/i.test(extType)) {
-            return `assets/media/[name]-[hash][extname]`;
-          }
-          return `assets/[name]-[hash][extname]`;
-        },
-      },
-    },
-  },
-})
+	plugins: [tailwindcss(), react()],
+	assetsInclude: ["**/*.mp4"],
+	build: {
+		assetsDir: "assets",
+		rollupOptions: {
+			output: {
+				assetFileNames: (assetInfo) => {
+					const info = assetInfo.name?.split(".") || [];
+					const extType = info[info.length - 1];
+					if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+						return `assets/images/[name]-[hash][extname]`;
+					}
+					if (/mp4|webm|ogg|mp3|wav|flac|aac/i.test(extType)) {
+						return `assets/media/[name]-[hash][extname]`;
+					}
+					return `assets/[name]-[hash][extname]`;
+				},
+			},
+		},
+	},
+});
